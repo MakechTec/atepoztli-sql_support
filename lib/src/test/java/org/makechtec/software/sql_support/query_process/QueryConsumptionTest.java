1722eebc5b25243daf937c77eca511164a94cf40
Angel Dominguez <angeldomp49@gmail.com>
2023-04-21 12:31:13 -0600

    added test for prepared

--- lib/src/test/java/org/makechtec/software/sql_support/query_process/QueryConsumptionTest.java
+++ lib/src/test/java/org/makechtec/software/sql_support/query_process/QueryConsumptionTest.java
@@ -4,6 +4,7 @@ import lombok.extern.java.Log;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.makechtec.software.sql_support.ConnectionInformation;
+import org.makechtec.software.sql_support.query_process.statement.ParamType;
 import org.makechtec.software.sql_support.query_process.statement.StatementInformation;
 
 import java.util.ArrayList;
@@ -66,6 +67,42 @@ public class QueryConsumptionTest {
 
         assertFalse(results.isEmpty());
 
+    }
+
+    @Test
+    public void testPreparedUpdate(){
+        var statementInformation =
+                StatementInformation.builder()
+                        .setQueryString("SELECT * FROM names WHERE name = ?;")
+                        .setPrepared(true)
+                        .addParamAtPosition(1, "test", ParamType.TYPE_STRING)
+                        .build();
+
+        var caller = new QueryCaller(connectionInformation, statementInformation);
+
+        caller.callUpdate();
+    }
+
+    @Test
+    public void testPreparedCall(){
+        var statementInformation =
+                StatementInformation.builder()
+                        .setQueryString("SELECT * FROM names WHERE name = ?;")
+                        .setPrepared(true)
+                        .addParamAtPosition(1, "test", ParamType.TYPE_STRING)
+                        .build();
+
+        var caller = new QueryCaller(connectionInformation, statementInformation);
+
+        var results = new ArrayList<String>();
+
+        caller.call( resultSet -> {
+            while(resultSet.next()){
+                results.add(resultSet.getString("name"));
+            }
+        });
+
+        assertFalse(results.isEmpty());
     }
 
 }
