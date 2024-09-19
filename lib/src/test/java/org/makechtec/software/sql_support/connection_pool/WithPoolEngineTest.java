d753f5fb5cd3521579efd1c0ba720553963bff5b
Angel Dominguez Plata <angeldomp49@gmail.com>
2024-09-19 13:57:25 -0600

    feat(version): Version 2.4.0
    Added test to the connection pool functionality, it will be under testing

--- lib/src/test/java/org/makechtec/software/sql_support/connection_pool/WithPoolEngineTest.java
+++ lib/src/test/java/org/makechtec/software/sql_support/connection_pool/WithPoolEngineTest.java
@@ -1,12 +1,16 @@
 package org.makechtec.software.sql_support.connection_pool;
 
 import org.junit.jupiter.api.Test;
+import org.makechtec.software.sql_support.ConnectionInformation;
 import org.makechtec.software.sql_support.connection_pool.mysql.MySQLPooledConnection;
+import org.makechtec.software.sql_support.connection_pool.mysql.MySQLPooledConnectionCreator;
+import org.makechtec.software.sql_support.mysql.MysqlEngine;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -127,5 +131,104 @@ class WithPoolEngineTest {
 
         return connectionMock;
     }
+
+    @Test
+    void realConnection() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+        if(Objects.isNull(System.getenv("isRealConnection")) || !System.getenv("isRealConnection").equals("true")) {
+            System.out.print("Disabled realConnection() test because is not enabled the isRealConnection env property to true");
+            return;
+        }
+
+        var connectionInformation = new ConnectionInformation(
+                "test",
+                "test",
+                "localhost",
+                "3306",
+                "test"
+        );
+
+        pooledConnection(connectionInformation);
+        normalConnection(connectionInformation);
+    }
+
+    private static void pooledConnection(ConnectionInformation connectionInformation) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        var pool = new ConnectionPool(4, new MySQLPooledConnectionCreator(connectionInformation));
+
+        pool.boot();
+
+        var initialTime = System.currentTimeMillis();
+
+        var result =
+                (new WithPoolEngine<Dto>(pool))
+                        .queryString("SELECT id, name FROM test")
+                        .run(resultSet -> {
+                            resultSet.next();
+
+                            return new Dto(resultSet.getInt("id"), resultSet.getString("name"));
+                        });
+
+        repeatPooled(6, pool);
+
+        var endTime = System.currentTimeMillis();
+
+        System.out.println("taked time: "+ (endTime - initialTime));
+
+        assertEquals(1, result.id);
+        assertEquals("john", result.name);
+    }
+
+    private static void repeatPooled(int times, ConnectionPool pool) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        for (int i = 0; i < times; i++) {
+            (new WithPoolEngine<Dto>(pool))
+                    .queryString("SELECT id, name FROM test")
+                    .run(resultSet -> {
+                        resultSet.next();
+
+                        return new Dto(resultSet.getInt("id"), resultSet.getString("name"));
+                    });
+        }
+
+    }
+
+    private static void normalConnection(ConnectionInformation connectionInformation){
+
+
+        var initialTime = System.currentTimeMillis();
+
+        var result =
+            (new MysqlEngine<Dto>(connectionInformation))
+                    .queryString("SELECT id, name FROM test")
+                    .run(resultSet -> {
+                        resultSet.next();
+
+                        return new Dto(resultSet.getInt("id"), resultSet.getString("name"));
+                    });
+
+        repeatNormal(6, connectionInformation);
+
+        var endTime = System.currentTimeMillis();
+
+        System.out.println("taked time: "+ (endTime - initialTime));
+
+        assertEquals(1, result.id);
+        assertEquals("john", result.name);
+
+    }
+
+    private static void repeatNormal(int times, ConnectionInformation connectionInformation){
+        for (int i = 0; i < times; i++) {
+            (new MysqlEngine<Dto>(connectionInformation))
+                    .queryString("SELECT id, name FROM test")
+                    .run(resultSet -> {
+                        resultSet.next();
+
+                        return new Dto(resultSet.getInt("id"), resultSet.getString("name"));
+                    });
+        }
+    }
+
+    record Dto(int id, String name) {}
+
 
 }
--- lib/src/test/java/org/makechtec/software/sql_support/mysql/ConnectionMysqlTest.java
+++ lib/src/test/java/org/makechtec/software/sql_support/mysql/ConnectionMysqlTest.java
@@ -3,18 +3,26 @@ package org.makechtec.software.sql_support.mysql;
 import org.junit.jupiter.api.Test;
 import org.makechtec.software.sql_support.ConnectionInformation;
 
+import java.util.Objects;
+
 public class ConnectionMysqlTest {
 
     @Test
     public void testConnection() {
+
+        if(Objects.isNull(System.getenv("isRealConnection")) || !System.getenv("isRealConnection").equals("true")) {
+            System.out.print("Disabled realConnection() test because is not enabled the isRealConnection env property to true");
+            return;
+        }
+
         (new MysqlEngine<Void>(new ConnectionInformation(
-                "",
-                "",
-                "",
-                "",
-                ""
+                "test",
+                "test",
+                "localhost",
+                "3306",
+                "test"
         )))
-                .queryString("SELECT * FROM boms;")
+                .queryString("SELECT id FROM test;")
                 .run(resultSet -> {
                     while (resultSet.next()) {
                         System.out.println(resultSet.getString("id"));
